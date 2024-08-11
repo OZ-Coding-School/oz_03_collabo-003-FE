2477a28efeb89c344d60726842ff00548c30511b
h22jul22 <heeju9402@gmail.com>
2024-08-11 23:46:34 +0900

    Feat: 홈페이지 인기있는 플랫폼 파트 UI 디자인(#5)

---
+++ src/types/type.ts
@@ -0,0 +1,11 @@
+export interface Content {
+  id: number;
+  name: string;
+  link: string;
+  home: string;
+  introduction: {
+    description: string;
+    available: null;
+  };
+  category: string;
+}

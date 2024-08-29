bfe1e655745590da5e44dda0dc3a0bef0fb1e0c2
h22jul22 <heeju9402@gmail.com>
2024-08-29 10:36:40 +0900

    카드 클릭시 상세페이지 이동 등 자잘한 기능 추가

--- src/components/specific/MainPopularCard.tsx
+++ src/components/specific/MainPopularCard.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { GoHeart } from 'react-icons/go';
-//import { GoHeartFill } from "react-icons/go";
+import { Link, useNavigate } from 'react-router-dom';
+import BtnHeart from '../common/button/BtnHeart';
 
 interface MainPopularCardProps {
   id: number;
@@ -11,15 +10,21 @@ interface MainPopularCardProps {
   site_description: string;
 }
 
-const MainPopularCard: React.FC<MainPopularCardProps> = ({ title, site_url, thumbnail, site_description }) => {
+const MainPopularCard: React.FC<MainPopularCardProps> = ({ id, title, site_url, thumbnail, site_description }) => {
+  const navigate = useNavigate();
   return (
     <div className='relative my-2 flex items-center justify-center'>
       <img className='rounded-sm' src={thumbnail} alt={title} />
       <div className='absolute left-0 top-0 flex h-full w-full flex-col justify-between p-2 md:p-3'>
         <div className='flex justify-between'>
-          <div className='truncate text-[10px] font-bold sm:text-xs md:text-sm lg:text-base xl:text-lg'>{title}</div>
+          <div
+            className='cursor-pointer truncate text-[10px] font-bold sm:text-xs md:text-sm lg:text-base xl:text-lg'
+            onClick={() => navigate(`/contents/${id}`)}
+          >
+            {title}
+          </div>
           <div className='flex items-center'>
-            <GoHeart className='hidden cursor-pointer font-bold sm:block sm:text-xs md:text-sm lg:text-base xl:text-lg 2xl:text-xl' />
+            <BtnHeart className='hidden cursor-pointer font-bold sm:block sm:text-xs md:text-sm lg:text-base xl:text-lg 2xl:text-xl' />
           </div>
         </div>
         <div className='flex items-center sm:justify-end md:justify-between md:gap-1 lg:gap-2'>

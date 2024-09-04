26dc23800023487f29e3783cb1bb4b889cecc7c2
h22jul22 <heeju9402@gmail.com>
2024-09-04 23:46:25 +0900

    content data type 수정

--- src/pages/OwnerSiteDetailPage.tsx
+++ src/pages/OwnerSiteDetailPage.tsx
@@ -13,32 +13,33 @@ const OwnerSiteDetailPage = () => {
   //const baseUrl = import.meta.env.VITE_API_URL;
 
   // 사이트 상세정보 조회
-  // const fetchContentData = async () => {
-  //   try {
-  //     const response = await axios.get(`${baseUrl}/contents/${contentId}`, {
-  //       withCredentials: true,
-  //     });
-  //     console.log('콘텐츠 정보 조회 성공', response);
-  //     setContent(response.data);
-  //   } catch (error) {
-  //     if (error instanceof AxiosError && error.response) {
-  //       switch (error.response.status) {
-  //         case 500:
-  //           return console.error('server error', error);
-  //         case 400:
-  //           return console.error('bad request', error);
-  //         default:
-  //           return console.error(error);
-  //       }
-  //     } else {
-  //       console.error(error);
-  //       alert('일시적인 문제가 발생했습니다. 서비스 이용에 불편을 드려 죄송합니다.');
-  //     }
-  //   }
-  // };
+  const fetchContentData = async () => {
+    setContent(contentData);
+    // try {
+    //   const response = await axios.get(`${baseUrl}/contents/${contentId}`, {
+    //     withCredentials: true,
+    //   });
+    //   console.log('콘텐츠 정보 조회 성공', response);
+    //   setContent(response.data);
+    // } catch (error) {
+    //   if (error instanceof AxiosError && error.response) {
+    //     switch (error.response.status) {
+    //       case 500:
+    //         return console.error('server error', error);
+    //       case 400:
+    //         return console.error('bad request', error);
+    //       default:
+    //         return console.error(error);
+    //     }
+    //   } else {
+    //     console.error(error);
+    //     alert('일시적인 문제가 발생했습니다. 서비스 이용에 불편을 드려 죄송합니다.');
+    //   }
+    // }
+  };
 
   useEffect(() => {
-    setContent(contentData);
+    fetchContentData();
   }, []);
 
   return contentId && content ? (
